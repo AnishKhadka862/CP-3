778aaaac8992f67c2ee86fb31c841d940fb29e8f
Anish Khadka <akhadka2@sfsu.edu>
2023-05-04 12:12:47 -0700

    Update dgemv-vectorized.cpp

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,4 +1,6 @@
-const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
+#include <omp.h>
+
+const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply using OpenMP.";
 
 /*
  * This routine performs a dgemv operation
@@ -7,10 +9,15 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
+   int num_threads = omp_get_max_threads();
+
+   #pragma omp parallel for num_threads(num_threads)
    for (int i = 0; i < n; i++) {
-      y[i] = 0.0;
+      double sum = 0.0;
       for (int j = 0; j < n; j++) {
-         y[i] += A[i*n + j] * x[j];
+         sum += A[i*n+j] * x[j];
       }
+      #pragma omp atomic
+      y[i] += sum;
    }
 }
